fa67a92de7d9101b9878025c4925bdc048aa8db7
adrian_wilgenhoff <adrian.wilgenhoff.ed@gmail.com>
2025-10-30 01:04:10 -0300

    first commit

---
+++ tpEspecial/mantenimiento-service/src/test/java/edu/tudai/arq/MantenimientoServiceApplicationTest.java
@@ -0,0 +1,14 @@
+package edu.tudai.arq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MantenimientoServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
---
+++ tpEspecial/parada-service/src/test/java/edu/tudai/arq/paradaservice/ParadaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tudai.arq.paradaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParadaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
